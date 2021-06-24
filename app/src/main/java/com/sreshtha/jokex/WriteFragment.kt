6dbcc8e3bba1f728cf2c1e505690232bd4b9d26a
Sreshtha <sreshtha.mehrotra@gmail.com>
2021-06-24 18:12:06 +0530

    Designed Write Fragment

--- app/src/main/java/com/sreshtha/jokex/WriteFragment.kt
+++ app/src/main/java/com/sreshtha/jokex/WriteFragment.kt
@@ -10,7 +10,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.firebase.storage.FirebaseStorage
 import com.sreshtha.jokex.databinding.FragmentWriteBinding
+
 
 class WriteFragment : Fragment() {
 
@@ -26,6 +28,8 @@ class WriteFragment : Fragment() {
         return binding.root
 
     }
+
+
 
 
 }
